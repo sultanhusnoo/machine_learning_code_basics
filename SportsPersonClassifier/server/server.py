faea5dda7d17e561e93d0643c1f37d8a130763e5
sultan <sultan.husnoo@gmail.com>
2021-12-28 17:03:46 +0400

    all working

--- SportsPersonClassifier/server/server.py
+++ SportsPersonClassifier/server/server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/classify_image", methods = ['GET', 'POST'])
 def classify_image():
     image_data = request.form['image_data']
-
+    
     response = jsonify(util.classify_image(image_data))
     response.headers.add('Access-Control-Allow-Origin','*')
     return response
